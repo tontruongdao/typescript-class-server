92b4de8b0a9e75ac23f4993c1c975683ff855976
Truong Dao <ton.truong.dao@outlook.com>
2021-04-17 16:05:28 -0400

    Added form and bodyParser to loginRoute

--- build/index.js
+++ build/index.js
@@ -5,7 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var loginRoute_1 = require("./routes/loginRoute");
+var body_parser_1 = __importDefault(require("body-parser"));
 var app = express_1.default();
+app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use(loginRoute_1.router);
 app.listen(4000, function () {
     console.log("Listening from port 4000");
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express, { Response, Request } from 'express';
 import { router } from './routes/loginRoute'
+import bodyParser from 'body-parser'
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended:true }))
 
 app.use(router)
 
