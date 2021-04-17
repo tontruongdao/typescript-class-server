75c3da35f8d128fe505ab7f892deed53b4a1cd88
Truong Dao <ton.truong.dao@outlook.com>
2021-04-17 15:56:15 -0400

    Added express router in project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('hello world')
+import express, { Response, Request } from 'express';
+import { router } from './routes/loginRoute'
+
+const app = express();
+
+app.use(router)
+
+app.listen(4000, () => {
+    console.log(`Listening from port 4000`)
+})
