58e257f59ad85b0de6376a6206823152f02a027b
Truong Dao <ton.truong.dao@outlook.com>
2021-04-17 16:40:49 -0400

    Added interface to Request type

--- src/routes/loginRoute.ts
+++ src/routes/loginRoute.ts
@@ -1,5 +1,9 @@
 import { Router, Response, Request } from 'express'
 
+interface RequestWithBody extends Request {
+    body: { [key: string]: string | undefined }
+}
+
 const router = Router();
 
 router.get('/login', (req: Request, res: Response) => {
@@ -19,10 +23,14 @@ router.get('/login', (req: Request, res: Response) => {
 })
 
 
-router.post('/login', (req: Request, res: Response) => {
+router.post('/login', (req: RequestWithBody, res: Response) => {
     const { email, password } = req.body;
 
-    res.send(email + password)
+    if(email) {
+        res.send(email.toUpperCase()) 
+    } else {
+        res.send('You must provide an email!')
+    }
 })
 
 export { router }
